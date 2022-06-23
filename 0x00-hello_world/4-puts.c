86f73c52175ee3fccca13bc581565fbf62b66c4d
MphomaM <matsekemphoma@yahoo.com>
2022-06-23 16:56:29 +0000

    My fifth C hello world commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- *main - Prints "Programming is like building a multilingual puzzle, 
-                 followed by a new line.
+ *main - entry point
  *
- *Return: Alwalys 0.
+ *Return:  0 after printing the function
  */
 int main(void)
 {
