a58249aef9e02e4f329ed4bfee6ed0e830311432
MphomaM <matsekemphoma@yahoo.com>
2022-07-01 14:31:52 +0000

    My eleventh C variables, if, else and while commit

---
+++ 0x01-variables_if_else_while/100-print_combs3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int c, i;
+
+c = i = '0';
+
+for (c = '0'; c <= '9'; i++)
+{
+putchar(c);
+putchar(i);
+
+if ((c != '9') || (c == '9' && i != '9'))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+
+putchar('\n');
+
+return (0);
+}
