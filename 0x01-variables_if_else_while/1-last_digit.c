0429beda429c07973c1a8bfa1ec91e2d527a398c
MphomaM <matsekemphoma@yahoo.com>
2022-07-01 13:07:27 +0000

    My second C variables, if, else and while commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,15 +1,15 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
- * Return: Always (success)
+ * Return: Always 0 (Success/correct)
  */
 int main(void)
+
 {
-int n, lastd;
+int n; lastd;
 
 srand(time(0));
 n = rand() - RAND_MAX /2;
