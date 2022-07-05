c31bce2f310124c27aaa5415f9923377ae3b4dea
MphomaM <matsekemphoma@yahoo.com>
2022-07-05 09:32:03 +0000

    My second C functions and nested loops commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet, in lowercase, folowed by a new line
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+_putchar(alphabet);
+}
+
+_putchar('\n');
+
+}
