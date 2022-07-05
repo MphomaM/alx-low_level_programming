0febdbe1a62b908053de93c685cdd3905be53087
MphomaM <matsekemphoma@yahoo.com>
2022-07-05 12:10:31 +0000

    My eighth C functions and nested loops commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,7 +8,7 @@ int print_last_digit(int n)
 {
 int last_digit;
 
-if (n > 0)
+if (n < 0)
 {
 last_digit = (-1 * (n % 10));
 _putchar (last_digit + '0');
@@ -21,4 +21,5 @@ last_digit = (n % 10);
 _putchar (last_digit + '0');
 return (last_digit);
 }
+
 }
