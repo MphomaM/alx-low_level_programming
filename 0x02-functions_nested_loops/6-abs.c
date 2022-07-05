294c76694b4e4ccc4a03837877660829e293dfca
MphomaM <matsekemphoma@yahoo.com>
2022-07-05 10:38:25 +0000

    My seventh C functions and nested loops commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - compute the absolute value of an integer
+ * @n: the int to check
+ * Return: the absolute value of the integer.
+ */
+int _abs(int n)
+{
+
+if (n >= 0)
+return (n);
+else
+return (-n);
+
+}
