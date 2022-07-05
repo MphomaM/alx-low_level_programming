0346d4c3f5560bfd57a4504da36ad4b668a13817
MphomaM <matsekemphoma@yahoo.com>
2022-07-05 18:50:34 +0000

    My thirteenth C functions and nested loops commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,11 +1,11 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_times_table - prints the n times table, starting with 0
  * @n: number of the times table
  */
 void print_times_table(int n)
+int main(void)
 {
 int i, j, k;
 
