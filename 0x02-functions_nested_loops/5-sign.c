cab2fd5f46984156e97ddb891d63add8794f320d
MphomaM <matsekemphoma@yahoo.com>
2022-07-05 11:58:30 +0000

    My sixth C funtions and nested loops commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_sign - prints the sign of a number
  * @n: the int to check
@@ -23,6 +24,6 @@ return (0);
 else (n < 0)
 {
 _putchar('-');
-return (-1)
+return (-1);
 }
 }
