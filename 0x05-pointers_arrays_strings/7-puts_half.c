7b0abf16d61ec5e0b9e083bd11ee8edd981cac9a
MphomaM <matsekemphoma@yahoo.com>
2022-07-11 08:24:18 +0000

    My seventh C pointers, arrays and strings commit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,21 +11,21 @@ int len, n, k
 
 len = 0;
 
-wwhile (str[len] != '\0')
+while (str[len] != '\0')
 {
-len++
+len++;
 }
 
 if (len % 2 == 0)
 {
-for (i = len / 2; str[k] != '\0'; k++)
+for (k = len / 2; str[k] != '\0'; k++)
 {
 _putchar(str[k]);
 }
 }
 else if (len % 2)
 {
-for (n = (len -1) / 2; n < len -1; n++)
+for (n = (len - 1) / 2; n < len - 1; n++)
 {
 _putchar(str[n + 1]);
 }
