46103cd6d43b01766fcbc607ecd93e0267130858
MphomaM <matsekemphoma@yahoo.com>
2022-07-13 20:21:00 +0000

    My eighth more C pointers, arrays and strings commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,7 +12,7 @@ int lower_case[] = {97, 101, 111, 116, 108};
 int upper_case[] = {65, 69, 79, 84, 76};
 int numbers[] = {52, 51, 48, 55, 49};
 
-while (*(s + count) != '\0'
+while (*(s + count) != '\0')
 {
 for (k = 0; k < 5; k++)
 {
