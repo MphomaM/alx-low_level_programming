50bffa5cbb7bab87804ef1f5cfb015106dc02ae6
MphomaM <matsekemphoma@yahoo.com>
2022-07-13 19:42:37 +0000

    My fifth more C pointers, arrays and strings commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ *reverse_array - reverses the content of an array of integers
+ *@a: an array of integers
+ *@n: number of elements of the array
+ *Return: returns void
+ */
+
+void reverse_array(int *a, int n)
+
+{
+int *start_c, *end_c, c;
+int k;
+
+start_c = a;
+end_c = a;
+
+for (k = 0; k < n - 1; k++)
+{
+end_c++;
+}
+
+for (k = 0; k < n /2; k++)
+{
+
+c = *end_c;
+*end_c = *start_c;
+*start_c = c;
+
+start_c++;
+end_c--;
+}
+
+}
