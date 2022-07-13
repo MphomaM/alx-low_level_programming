591ccdc258527a5b7045e39f6f85982589f69825
MphomaM <matsekemphoma@yahoo.com>
2022-07-13 18:15:49 +0000

    My more C pointers, arrays and strings main commit

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,5 +10,5 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-int _putchar(char);
+int _putchar(char c);
 
