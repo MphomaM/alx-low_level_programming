7e5ce7a5b53edb34a35e549a38416e83f9e504b4
MphomaM <matsekemphoma@yahoo.com>
2022-07-18 11:17:38 +0000

    My fourth even more C pointers, arrays and strings commit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * strspn - gets the length of a prefix substring
+ * _strspn - gets the length of a prefix substring
  * @s: string to evaluate
  * @accept: string containing the list of characters to match in s
  *
