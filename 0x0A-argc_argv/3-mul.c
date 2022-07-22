6875ff7e370ccd7e481ddf46a4e697798f8f30d6
MphomaM <matsekemphoma@yahoo.com>
2022-07-22 08:25:48 +0000

    My fourth C argc, argv commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the multiplication of two integers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if true, 1 if false
+ */
+int main(int argc, char *argv[])
+{
+int j, k;
+if (argc == 3)
+{
+j = atoi(argv[1]);
+k = atoi(argv[2]);
+printf("%d\n", j *k);
+return (0);
+}
+printf("Error\n");
+return (1);
+}
