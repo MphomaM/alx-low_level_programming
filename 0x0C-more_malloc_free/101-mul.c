fe4a67669cc9dfbf1374a94d7c5bbb584ad25cd9
MphomaM <matsekemphoma@yahoo.com>
2022-07-28 11:07:52 +0000

    My sixth C more malloc, free commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -11,19 +11,15 @@ int main(int argc, char *argv[])
 unsigned long mul;
 int k, l;
 if (argc != 3)
-{
-printf("Error\n");
-exit(98);
-}
+{ printf("Error\n");
+exit(98); }
 for (k = 1; k < argc; k++)
 {
 for (l = 0; argv[k][l] != '\0'; l++)
 {
 if (argv[k][l] > 57 || argv[k][l] < 48)
-{
-printf("Error\n");
-exit(98);
-}
+{ printf("Error\n");
+exit(98); }
 }
 
 }
