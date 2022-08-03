fc136c85e13729dc5df75c9796897cc943c732f5
MphomaM <matsekemphoma@yahoo.com>
2022-08-03 07:43:17 +0000

    My first C function pointers commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @f: pointer to the printing function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (!name || !f)
+return;
+f(name);
+}
