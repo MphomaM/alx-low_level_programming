b640b51fd08d7f86bf2411eeb26f7abbae5eb12a
MphomaM <matsekemphoma@yahoo.com>
2022-08-03 11:18:08 +0000

    My third C function pointers commit

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,13 +14,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 int k;
 
-if (array == NULL || cmp == NULL)
-return (-1);
-
+if (array && cmp)
+{
 for (k = 0; k < size; k++)
 {
-if (cmp(array[k] != 0)
+if (cmp(array[k]) != 0)
 return (k);
+}
 }
 
 return (-1);
