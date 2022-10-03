d2eb3e9966e489f82a22bb3c9c567f4dbcd0a696
MphomaM <106753614+MphomaM@users.noreply.github.com>
2022-10-03 13:42:45 +0200

    Create 2-strncpy.c

---
+++ 0x18-dynamic_libraries/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *_strncpy - copies a string
+ *@dest: A pointer to a character that will be changed
+ *@src: A pointer to a character that will be changed
+ *@n: value
+ *Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+
+{
+int l;
+
+l = 0;
+while (l < n && src[l] != '\0')
+{
+dest[l] = src[l];
+l++;
+}
+
+while (l < n)
+{
+dest[l] = '\0';
+l++;
+}
+
+return (dest);
+
+}
