e1232b2f2d8ba609cf8ad104606eeed9d80e6375
MphomaM <106753614+MphomaM@users.noreply.github.com>
2022-10-03 13:59:58 +0200

    Create 9-strcpy.c

---
+++ 0x18-dynamic_libraries/9-strcpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies a string pointed to by @src, including the
+ * terminating null byte, to a buffer pointed to by @dest.
+ * @dest: A buffer to copy the string to.
+ * @src: The source string to copy.
+ *
+ * Return: A pointer to the destination string @dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+int len, k;
+
+len = 0;
+
+while (src[len] != '\0')
+{
+len++;
+}
+
+for (k = 0; k < len; k++)
+{
+dest[k] = src[k];
+}
+dest[k] = '\0';
+
+return (dest);
+}
